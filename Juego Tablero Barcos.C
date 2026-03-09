6f8adf4d1dc3de8d1d9dea8468b1a44ab6623f9a
NeriCUCEI <gabriel.neri5969@alumnos.udg.mx>
2026-03-09 11:25:34 -0600

    Update Juego Tablero Barcos.C

--- Juego Tablero Barcos.C
+++ Juego Tablero Barcos.C
@@ -1,58 +1,64 @@
 #include <stdio.h>
+#include <windows.h>
 
+void Mostrar_Tablero(int Tablero[3][3]) {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("[%d]", Tablero[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
-void Crear_Tablero(int filas, int columnas, int Tablero[]) {
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-           Tablero[i][j] = 0; 
+void Crear_Tablero(int Tablero[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+           Tablero[i][j] = 0;
         }
     }
 }
 
-void Poner_Barco(int Tablero[]) {
+int Poner_Barco(int Tablero[3][3]) {
     int origen_x_barco; int fila_barco;
-    printf("Origen en X del Barco grande: "); 
-    scandf("%d", &origen_x_barco);
+    printf("Columna de origen [X] del Barco grande: ");
+    scanf("%d", &origen_x_barco);
+    if (origen_x_barco < 0) {origen_x_barco *= -1;}
+    if (origen_x_barco > 1) {origen_x_barco = 1;}
     printf("Fila [Y] del Barco grande: ");
     scanf("%d", &fila_barco);
-    int coordenadas_barco[2][2] = {{origen_x_barco, fila_barco}, {(origen_x_barco + 1), fila_barco}};
+    int coordenadas_x_barco[2] = {origen_x_barco, origen_x_barco + 1};
     for (int x = 0; x < 2; x++) {
-        for (int y = 0; y < 2; y++) {
-            if (Tablero[coordenadas_barco[x,y], y])
-            Tablero[coordenadas_barco[x,y]
+        if (Tablero[fila_barco][coordenadas_x_barco[x]] != 0) {
+            printf("No puede estar encima del bote pequeño");
+            if (x > 0) {Tablero[fila_barco][coordenadas_x_barco[x] - 1] = 0;}
+            return 0;
         }
+        Tablero[fila_barco][coordenadas_x_barco[x]] = 1;
     }
+    return 1;
 }
 
-void configurar_Tablero(int filas, int columnas, int Tablero[]) {
-    int X, int Y; int fila_barco; int origen_x_barco; //int coordenadas_barco[2];
-    for (int i = 0; i < filas; i++) {
-        printf("Posicion X del Bote pequeño: ");
-        scanf("%d", &X);
-        printf("Posicion Y del Bote pequeño: ");
-        scanf("%d", &Y);
-        Tablero[X, Y] = 1;
-        printf("Origen en X del Barco grande: "); 
-        scandf("%d", &origen_x_barco);
-        printf("Fila del Barco grande: ");
-        scanf("%d", &fila_barco);
-        int coordenadas_barco[2][2] = {{origen_x_barco, fila_barco}, {(origen_x_barco + 1), fila_barco}};
-        for (int x = 0; x < 2; x++) {
-            Tablero 
-            for (int y = 0; y < 2; y++) {
-                Tab
-            }
-        }
+void configurar_Tablero(int Tablero[3][3]) {
+    int X; int Y; int fila_barco; int origen_x_barco;
+    printf("Columna [Posicion X] del Bote pequeño: ");
+    scanf("%d", &X);
+    printf("Fila [Posicion Y] del Bote pequeño: ");
+    scanf("%d", &Y);
+    Tablero[X][Y] = 1; int Posicion_Bote[2] = {X, Y};
+    while (1) {
+        int exito = Poner_Barco(Tablero);
+        if (exito == 1) {break;}
     }
 }
 
 int main() {
-    int Filas = 3; int Columnas = 3;
-    int Tablero_1 = [Filas, Columnas]; int Tablero_2 = [Filas, Columnas];
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            printf("[Fila %d, Columna %d] =  %d\n", i, j, Tablero[i][j]);
-        }
-    }
+    SetConsoleOutputCP(65001);
+    int Tablero_1[3][3]; int Tablero_2[3][3];
+    Crear_Tablero(Tablero_1);
+    configurar_Tablero(Tablero_1);
+    Mostrar_Tablero(Tablero_1);
     return 0;
 }
+
