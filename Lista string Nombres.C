aca44bf459d5b0d67633b9e19e703308068e2c36
NeriCUCEI <gabriel.neri5969@alumnos.udg.mx>
2026-03-21 17:54:06 -0600

    Update Lista string Nombres.C

--- Lista string Nombres.C
+++ Lista string Nombres.C
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <windows.h>
+#include <locale.h>
 #include <string.h>
 
 void Listar_Nombres(char Nombres[10][20]) {
+    printf("\n");
     for (int i = 0; i < 10; i++) {
-            printf("%d ", strlen(Nombres[i]));
-            if (strlen(Nombres[i]) <= 0) {printf("\nNo hay nombre\n"); break;}
-            printf("%s", Nombres[i]);
+        if (strlen(Nombres[i]) <= 0) {break;}
+        printf("[%d] %s", i+1, Nombres[i]);
     }
 }
 
@@ -16,22 +16,29 @@ void Insertar_Nombre(char Nombres[10][20], char Nombre[20], int index) {
     }
 }
 
+void Default_Lista_Nombres(char Nombres[10][20]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 20; j++) {
+            Nombres[i][j] = '\0';
+        }
+    }
+}
+
 void Crear_Lista_Nombres(char Nombres[10][20]) {
     int n_Nombres; char Nombre[20];
     printf("¿Cuántos nombres vas a ingresar (El máximo es 10): ");
-    scanf("%d", &n_Nombres);
+    scanf("%d", &n_Nombres); getchar();
     if (n_Nombres > 10) {n_Nombres = 10;}
     for (int i = 0; i < n_Nombres; i++) {
-            printf("Nombre %d: ", i+1);
-            fgets(Nombre, sizeof(Nombre), stdin);
-            Insertar_Nombre(Nombres, Nombre, i);
-            printf("\n");
+        printf("Nombre %d: ", i+1);
+        fgets(Nombre, sizeof(Nombre), stdin);
+        Insertar_Nombre(Nombres, Nombre, i);
     }
 }
 
 int main() {
-    SetConsoleOutputCP(65001);
-    char Nombres[10][20];
+    setlocale(LC_ALL, "");
+    char Nombres[10][20]; Default_Lista_Nombres(Nombres);
     Crear_Lista_Nombres(Nombres);
     Listar_Nombres(Nombres);
     return 0;
